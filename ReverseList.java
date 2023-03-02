997ed8b86b0e2ecb52eadd4047ea79bb7c458521
athishsreeram <athishsreeram007@gmail.com>
2023-03-02 07:22:33 -0500

    Update ReverseList.java

--- ReverseList.java
+++ ReverseList.java
@@ -1,19 +1,23 @@
 public class ReverseList {
+
     public static void main(String[] args) {
-        Node n = new Node(1);
-        n.next = new Node(2);
+        Node node = new Node(1);
+        node.next = new Node (2);
+        node.next.next = new Node (3);
 
-        printList(n);
+        printNode(node);
 
-        Node revlist = reverseList(n);
-        printList(revlist);
+        printNode(reverseList(node));
+
 
     }
 
-    public static Node reverseList(Node head) {
+    private static Node reverseList(Node head) {
+
         Node prev = null;
         Node cur = head;
-        while (cur != null) {
+
+        while(cur != null){
             Node nextNode = cur.next;
             cur.next = prev;
             prev = cur;
@@ -23,12 +27,12 @@ public class ReverseList {
         return prev;
     }
 
-    public static void printList(Node cur) {
-        while (cur.next != null) {
+    private static void printNode(Node cur) {
+        while(cur !=null){
             System.out.println(cur.v);
             cur = cur.next;
         }
-        System.out.println(cur.v);
     }
+
 
 }
